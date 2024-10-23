7a0eb4b7b466e3858119ca16adf47114a9127f32
hiennq12 <hien.nq.hust@gmail.com>
2024-10-23 20:00:13 +0700

    get total money

---
+++ struct_modal/SpendMoney.go
@@ -0,0 +1,6 @@
+package struct_modal
+
+type SpendMoney struct {
+	Money float64 `json:"money"`
+	Note  string  `json:"note"`
+}
---
+++ util/util.go
@@ -0,0 +1,28 @@
+package util
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+)
+
+func GetDateFromFirstCell(day string) time.Time {
+	if len(day) < 2 {
+		day = fmt.Sprintf("0%v", day)
+	}
+	month := strconv.Itoa(int(time.Now().Month()))
+	if len(month) < 2 {
+		month = fmt.Sprintf("0%v", month)
+	}
+	dateStr := fmt.Sprintf("%v/%v/%v", day, month, time.Now().Year())
+	layout := "02/01/2006"
+	dateTime, err := time.Parse(layout, dateStr)
+	if err != nil {
+		log.Panic("Error when parse date time", err.Error())
+	}
+	dateStr = dateTime.Format("02/01/2006 15:04:05")
+
+	fmt.Println("Dasdauyduya: ", dateStr)
+	return dateTime
+}
